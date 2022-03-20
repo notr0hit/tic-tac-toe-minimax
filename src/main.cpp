c6747c3bdb0e7e8833d702e7b43797ef2812a2db
Rohit Khare <rohit97528@gmail.com>
2022-03-21 02:42:32 +0530

    implemented minimax algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
 #include <string>
 
+struct Move {
+  int r, c, score;
+};
+
 class Game {
   // player, computer, grid[3][3]
-  std::array<std::array<char, 5>, 5> board{{{' ', '|', ' ', '|', ' '},
-                                            {'-', '+', '-', '+', '-'},
-                                            {' ', '|', ' ', '|', ' '},
-                                            {'-', '+', '-', '+', '-'},
-                                            {' ', '|', ' ', '|', ' '}}};
-  char symbol = 'X';
+  char player = 'X';
+  char bot = 'O';
 
 public:
-  void Print() {
+  void Print(std::array<std::array<char, 5>, 5> board) {
     std::cout << "\n";
     for (int i = 0; i < 5; ++i) {
       for (int j = 0; j < 5; ++j) {
@@ -24,7 +25,14 @@ public:
   }
 
   void Play() {
-    this->Print();
+
+    std::array<std::array<char, 5>, 5> board{{{' ', '|', ' ', '|', ' '},
+                                              {'-', '+', '-', '+', '-'},
+                                              {' ', '|', ' ', '|', ' '},
+                                              {'-', '+', '-', '+', '-'},
+                                              {' ', '|', ' ', '|', ' '}}};
+    Print(board);
+    char symbol = 'X';
     while (true) {
       std::cout << "Enter a Symbol (X or O, X goes first): ";
       std::cin >> symbol;
@@ -34,27 +42,29 @@ public:
     }
 
     if (symbol == 'X') {
+      player = 'X', bot = 'O';
       while (true) {
-        Player('X');
-        Verdict("player");
+        Player(board);
+        Verdict(board, "player");
 
         std::cout << "Computer is playing...\n";
-        Computer('O');
-        Verdict("computer");
+        Computer(board);
+        Verdict(board, "computer");
       }
     } else {
+      player = 'O', bot = 'X';
       while (true) {
-        Computer('X');
-        Verdict("computer");
-
         std::cout << "Computer is playing...\n";
-        Player('O');
-        Verdict("player");
+        Computer(board);
+        Verdict(board, "computer");
+
+        Player(board);
+        Verdict(board, "player");
       }
     }
   }
 
-  void Player(char sym) {
+  void Player(std::array<std::array<char, 5>, 5> &board) {
     while (true) {
       int response;
       std::cout << "Enter an empty cell (1 - 9) : ";
@@ -63,37 +73,109 @@ public:
       int r = 2 * ((response - 1) / 3), c = 2 * ((response - 1) % 3);
       if (response >= 1 and response <= 9 and board[r][c] == ' ') {
         // std::cerr << r << " " << c << "\n";
-        board[r][c] = sym;
+        board[r][c] = player;
         break;
       }
     }
   }
 
-  void Computer(bool sym) {}
+  void Computer(std::array<std::array<char, 5>, 5> &board) {
 
-  std::string Check() {
-    std::string res = "lose";
+    Move best;
+    best.score = -10;
+    for (int r = 0; r < 5; r += 2) {
+      for (int c = 0; c < 5; c += 2) {
+        if (board[r][c] == ' ') {
+          board[r][c] = bot;
+          int score = MiniMax(board, "player", 0);
+          board[r][c] = ' ';
+
+          if (best.score < score) {
+            best.score = score;
+            best.r = r;
+            best.c = c;
+          }
+        }
+      }
+    }
+
+    board[best.r][best.c] = bot;
+  }
+
+  int MiniMax(std::array<std::array<char, 5>, 5> board, std::string who,
+              int depth) {
+
+    int res = Check(board);
+
+    if (res == 1 or res == -1 or res == 0)
+      return res;
+
+    if (who == "computer") {
+      int max_score = -10;
+      for (int r = 0; r < 5; r += 2) {
+        for (int c = 0; c < 5; c += 2) {
+          if (board[r][c] == ' ') {
+            board[r][c] = bot;
+            int score = MiniMax(board, "player", depth + 1);
+            max_score = std::max(max_score, score);
+            board[r][c] = ' ';
+          }
+        }
+      }
+      return max_score;
+
+    } else if (who == "player") {
+      int min_score = 10;
+      for (int r = 0; r < 5; r += 2) {
+        for (int c = 0; c < 5; c += 2) {
+          if (board[r][c] == ' ') {
+            board[r][c] = player;
+            int score = MiniMax(board, "computer", depth + 1);
+            min_score = std::min(min_score, score);
+            board[r][c] = ' ';
+          }
+        }
+      }
+      return min_score;
+    }
+
+    return 0;
+  }
+
+  int Check(std::array<std::array<char, 5>, 5> board) {
     // check each row;
     for (int r = 0; r < 5; r += 2) {
       if (board[r][0] == board[r][2] and board[r][0] == board[r][4]) {
-        if (board[r][0] == 'X' or board[r][0] == 'O')
-          res = "win";
+        if (board[r][0] == player) {
+          return -1;
+        }
+        if (board[r][0] == bot) {
+          return 1;
+        }
       }
     }
 
     // check each column
     for (int c = 0; c < 5; c += 2) {
       if (board[0][c] == board[2][c] and board[0][c] == board[4][c]) {
-        if (board[0][c] == 'X' or board[0][c] == 'O')
-          res = "win";
+        if (board[0][c] == player) {
+          return -1;
+        }
+        if (board[0][c] == bot) {
+          return 1;
+        }
       }
     }
 
     // check each diagonal
-    if (board[0][0] == board[2][2] and board[0][0] == board[4][4] ||
-        board[0][4] == board[2][2] and board[0][4] == board[4][0]) {
-      if (board[2][2] == 'X' or board[2][2] == 'O')
-        res = "win";
+    if ((board[0][0] == board[2][2] and board[2][2] == board[4][4]) ||
+        (board[0][4] == board[2][2] and board[2][2] == board[4][0])) {
+      if (board[2][2] == player) {
+        return -1;
+      }
+      if (board[2][2] == bot) {
+        return 1;
+      }
     }
 
     // Check Tie
@@ -103,33 +185,30 @@ public:
         count += (board[r][c] != ' ');
 
     if (count == 9) {
-      res = "tie";
+      return 0;
     }
 
-    return res;
+    return 100;
   }
 
-  void Verdict(std::string who) {
+  void Verdict(std::array<std::array<char, 5>, 5> board, std::string who) {
 
-    this->Print();
+    Print(board);
 
-    std::string res = Check();
+    int res = Check(board);
 
-    if (who == "player") {
-      if (res == "win") {
+    if (res == 1) {
+      if (who == "player") {
         std::cout << "Congratulations, You Won!\n";
         exit(0);
       }
-    }
-
-    if (who == "computer") {
-      if (res == "win") {
+      if (who == "computer") {
         std::cout << "You lost, Better luck next time!\n";
         exit(0);
       }
     }
 
-    if (res == "tie") {
+    if (res == 0) {
       std::cout << "Game tied!\n";
       exit(0);
     }
